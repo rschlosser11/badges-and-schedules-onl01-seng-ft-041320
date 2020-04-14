9d42014caca2d7e604721a55a4edfe6e8a1bdc74
global-event-2762 <rosalinschlosser@gmail.com>
2020-04-14 15:32:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    attendees.collect{|name| badge_maker(name)};
+end
+
+def assign_rooms(attendees)
+  room_number = 1;
+  index = 0;
+  room_assignments = [];
+  until index == attendees.length do
+    room_assignments << "Hello, #{attendees[index]}! You'll be assigned to room #{room_number}!"
+    index +=
+    room_number +=
+  end
+  room_assignments
+end
